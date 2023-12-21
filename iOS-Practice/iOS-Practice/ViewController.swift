6f6f27dbf6d20a20b7c556826274c3b47608d703
shinzz410 <tlswldnjs410@gmail.com>
2023-12-21 17:54:31 +0900

    init

---
+++ iOS-Practice/iOS-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Practice
+//
+//  Created by 신지원 on 12/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
