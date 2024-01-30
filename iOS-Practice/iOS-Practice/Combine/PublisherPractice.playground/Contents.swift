143a92fc481b8c502ffef538ca0b06efc66081a4
shinzz410 <tlswldnjs410@gmail.com>
2024-01-30 22:44:08 +0900

    Publisher

---
+++ iOS-Practice/iOS-Practice/Combine/PublisherPractice.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Combine
+
+var myIntArrayPublisher: Publishers.Sequence<[Int], Never> = [1,2,3].publisher
+
+myIntArrayPublisher.sink(receiveCompletion: { completion in 
+    switch completion {
+    case .finished:
+        print("Complete")
+    case .failure(let error):
+        print(error)
+    }
+}, receiveValue: { receivedValue in
+    print(receivedValue)
+})
